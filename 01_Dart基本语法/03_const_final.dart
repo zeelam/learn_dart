dd95e2f144a33a6ffa63eccd22e0fa8107bc5a14
Zeelam <494453244@qq.com>
2020-04-12 19:27:13 +0800

    const&final

--- 01_Dart基本语法/03_const_final.dart
+++ 01_Dart基本语法/03_const_final.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 main(List<String> args) {
   // const 和 final都可以定义常量
   const name = "zeelam";
@@ -11,7 +13,20 @@ main(List<String> args) {
   // const const_name = getName(); // 不允许
   final final_name = getName(); // 允许
 
-  
+  // 共享对象 节省内存
+  const p1 = const Person();
+  const p2 = const Person();
+
+  print(identical(p1, p2)); // true
+
+  final p3 = Person();
+  final p4 = Person();
+  print(identical(p3, p4)); // false
+
+}
+
+class Person{
+  const Person();
 }
 
 String getName(){
