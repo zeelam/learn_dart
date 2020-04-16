8e479888fe4a6f09339205ae8985ce91ce533524
Zeelam <494453244@qq.com>
2020-04-17 00:14:42 +0800

    default value

--- 02_function/02_函数的参数.dart
+++ 02_function/02_函数的参数.dart
@@ -7,15 +7,32 @@ main(List<String> args) {
   printInfo2("zeelam", 18);
   printInfo2("zeelam", 18, 1.88);
 
+  printInfo3("zeelam");
+  printInfo3("zeelam", age: 20);
+  printInfo3("zeelam", age: 20, height: 1.80);
+
+  printInfo4("zeelam");
+  printInfo4("zeelam", 20);
+  printInfo4("zeelam", 20, 1.80);
+
 }
 
 // 可选参数
 // 命名可选参数
-printInfo(String name,{int age, double height}){
+printInfo(String name, {int age, double height}){
   print("name = ${name}, age = ${age}, height = ${height}");
 }
 
 // 位置可选参数
 printInfo2(String name, [int age, double height]){
+  print("name = ${name}, age = ${age}, height = ${height}");
+}
+
+// 默认参数
+printInfo3(String name, {int age = 18, double height = 1.88}){
+  print("name = ${name}, age = ${age}, height = ${height}");
+}
+
+printInfo4(String name, [int age = 18, double height = 1.88]){
   print("name = ${name}, age = ${age}, height = ${height}");
 }
