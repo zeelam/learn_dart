1f99abf2bb5c977a416a19da9f42e259fc27b3b2
Zeelam <494453244@qq.com>
2020-04-17 23:10:45 +0800

    anonymous function

--- 02_function/04_ anonymous_function.dart
+++ 02_function/04_ anonymous_function.dart
@@ -1,3 +1,17 @@
 main(List<String> args) {
-  
+  var movies = ['模仿游戏', '盗梦空间', '星际穿越'];
+
+  // 使用有名字的函数进行遍历
+  printElement(item){
+    print(item);
+  }
+  movies.forEach(printElement);
+
+  // 使用匿名函数遍历
+  movies.forEach((item){
+    print(item);
+  });
+
+  movies.forEach((item) => print(item));
+
 }
