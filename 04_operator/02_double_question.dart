289ef38c974f32864f0a9acb899d0d5acf2bfe9a
Zeelam <494453244@qq.com>
2020-04-20 23:36:36 +0800

    double question mark

--- 04_operator/02_double_question.dart
+++ 04_operator/02_double_question.dart
@@ -7,4 +7,11 @@ main(List<String> args) {
   var name_2 = null;
   name_2 ??= "zeelam"; // 当name_2为null时，将zeelam赋值给name_2
   print(name_2);
+
+  // 条件运算符??
+  // var temp = "zeelam";
+  var temp = null;
+  var name_3 = temp ?? "zzl"; // temp不为null时，取temp的值，temp为null时，取??后面的值
+  print(name_3);
+
 }
