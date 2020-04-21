6c96bd76208b7a2a14291ff3f0055e41123720db
Zeelam <494453244@qq.com>
2020-04-21 23:19:42 +0800

    for&for in

---
+++ 05_flow_control/02_for_&_for_in.dart
@@ -0,0 +1,14 @@
+main(List<String> args) {
+  
+  // for
+  for(var i = 0; i < 10; i++){
+    print("i is ${i}");
+  }
+
+  // for in
+  var language_list = ["Java", "C", "C++", "Python", "dart"];
+  for (var language in language_list) {
+    print("item is ${language}");
+  }
+
+}
