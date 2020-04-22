c5ffcb48c7fc495db28e84512a90211de95303ec
Zeelam <494453244@qq.com>
2020-04-22 23:56:52 +0800

    switch_case

---
+++ 05_flow_control/03_switch_case.dart
@@ -0,0 +1,20 @@
+main(List<String> args) {
+  var direction = "north";
+  switch (direction) {
+    case "north":
+      print("north");
+      break;
+    case "east":
+      print("east");
+      break;
+    case "west":
+      print("west");
+      break;
+    case "south":
+      print("south");
+      break;
+    defalut:
+      print("other");
+  }
+
+}
